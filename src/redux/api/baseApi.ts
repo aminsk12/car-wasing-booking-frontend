0491a0177b5639859fbec743d13ec84118c20b95
amin sk <aminskeh786@gmail.com>
2024-10-20 20:15:45 +0530

    sum update

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,9 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    //baseUrl: "https://car-washing-system-backend.vercel.app/api",
-  baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://car-wash-booking-systam-backend-a-5.vercel.app/api",
+    //baseUrl: "http://localhost:5000/api",
   }),
-  tagTypes:["Service", "Slot", "User", "Review"],
+  tagTypes: ["Service", "Slot", "User", "Review"],
   endpoints: () => ({}),
 });
